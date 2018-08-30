55618aa915a0df3eebde72c3646946d4b2887187
Olisaer <42570202+Olisaer@users.noreply.github.com>
2018-08-30 11:55:22 +0000

    Add files via upload

---
+++ skonumer og verd.py
@@ -0,0 +1,60 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+>>> kona
+'Gunna'
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+hér er saga um karl og kona
+hvað heitir karlinn?jóhann
+hvað heitir konan?jóhanna
+hvað kostuðu skórnir5000 kr
+Traceback (most recent call last):
+  File "C:/Users/Esdeath/Desktop/python verkefni/verkefni.py", line 7, in <module>
+    kronur=int(input("hvað kostuðu skórnir"))
+ValueError: invalid literal for int() with base 10: '5000 kr'
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+hér er saga um karl og kona
+hvað heitir karlinn?jón
+hvað heitir konan?j
+hvað kostuðu skórnir200
+númer hvað eu skórnir?40
+Traceback (most recent call last):
+  File "C:/Users/Esdeath/Desktop/python verkefni/verkefni.py", line 9, in <module>
+    print(karl,"keypti skónúmer",skonumer,"fyrir",kona,"sem kostuðu",kronur,"krónur")
+NameError: name 'karl' is not defined
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+hér er saga um karl og kona
+hvað heitir karl?jon
+hvað heitir kona?johanna
+hvað kostuðu skórnir5000
+númer hvað eu skórnir?40
+Traceback (most recent call last):
+  File "C:/Users/Esdeath/Desktop/python verkefni/verkefni.py", line 9, in <module>
+    print(karl,"keypti skónúmer",skonumer,"fyrir",kona,"sem kostuðu",kronur,"krónur")
+NameError: name 'karl' is not defined
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+hér er saga um karl og kona
+hvað heitir karl?jon
+hvað heitir kona?johanna
+hvað kostuðu skórnir5000
+númer hvað eu skórnir?40
+jon keypti skónúmer 40 fyrir johanna sem kostuðu 5000 krónur
+en þá mundi hann að hún notar skónúmer 42
+>>> 
+======= RESTART: C:/Users/Esdeath/Desktop/python verkefni/verkefni.py =======
+hér er saga um karl og kona
+hvað heitir karl?jon
+hvað heitir kona?johanna
+hvað kostuðu skórnir5000
+númer hvað eu skórnir?40
+jon keypti skónúmer 40 fyrir johanna sem kostuðu 5000 krónur
+en þá mundi hann að hún notar skónúmer 42
+>>> 
+>>> 
